d79246031ce619a034abd4d1e8dcd2ae7c0f8d3e
Christine <papasc1@tcnj.edu>
2026-06-13 19:12:55 -0600

    edit background and add scroll up button

---
+++ ui.js
@@ -0,0 +1,12 @@
+
+// back to top button
+document.addEventListener('DOMContentLoaded', () => {
+  //const backToTop = document.getElementById('backToTop');
+  
+  //no error even if button is not implemented on page
+  //if (backToTop) {
+    //window.addEventListener('scroll', () => {
+      //backToTop.classList.toggle('visible', window.scrollY > 300);
+    //});
+  //}
+});
